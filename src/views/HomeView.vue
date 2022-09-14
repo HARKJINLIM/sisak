8402185912abc56dac2d5a4e17650b86f51cd49b
harkjinlim <harkjin.lim@gamil.com>
2022-09-15 07:26:42 +0900

    Progressbar

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -8,6 +8,14 @@
       <v-btn @click="toastTest4">Toast Warning</v-btn>
       <v-btn @click="toastTest5">Total Error</v-btn>
     </div>
+    <br>
+    <h1>progressbar</h1>
+    <div>
+      <v-btn @click="barTest1">Start</v-btn>
+      <v-btn @click="barTest2">Finish</v-btn>
+      <v-btn @click="barTest3">Fail</v-btn>
+    </div>
+
   </div>
 </template>
 
@@ -29,6 +37,15 @@
       },
       toastTest5() {
         throw new Error("Total error");
+      },
+      barTest1() {
+        this.$Progress.start();
+      },
+      barTest2() {
+        this.$Progress.finish();
+      },
+      barTest3() {
+        this.$Progress.fail();
       }
     },
   }
