deb69caf0b47c8c107b92e7f0e51b69b5a45ced0
harkjinlim <harkjin.lim@gamil.com>
2022-09-14 20:52:58 +0900

    Toast Plugin

---
+++ src/plugins-client/vue-toast.js
@@ -0,0 +1,16 @@
+import Vue from "vue";
+import Toast from "vue-toastification";
+import "vue-toastification/dist/index.css";
+
+const options = {
+  position : 'bottom-center',
+	timeout : 3000,
+};
+
+Vue.config.errorHandler = (e) => {
+    console.error(e);
+    if(Vue.$toast)
+        Vue.$toast.error(e.message);
+}
+
+Vue.use(Toast, options);
