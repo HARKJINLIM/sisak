c346fea8433073a3bf9f79bf5e82fe21a94b9fcb
harkjinlim <harkjin.lim@gamil.com>
2022-09-15 21:34:55 +0900

    Mixin

--- src/main.js
+++ src/main.js
@@ -7,6 +7,7 @@ import vuetify from './plugins/vuetify'
 import '@babel/polyfill'
 import 'roboto-fontface/css/roboto/roboto-fontface.css'
 import '@mdi/font/css/materialdesignicons.css'
+import titleMixin from './mixins/title-mixin';
 
 Vue.config.productionTip = false
 
@@ -15,6 +16,8 @@ export function createApp(ctx) {
 	const store = createStore();
 	sync(store, router);
 
+	Vue.mixin(titleMixin);
+	
 	const app = new Vue({
 		data: { url: ctx ? ctx.url : '' },
 		router,
