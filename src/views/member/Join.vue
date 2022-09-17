e35d3e00d11d0ee37f8081bdeeb80550ca9b66d0
harkjinlim <harkjin.lim@gamil.com>
2022-09-17 10:01:23 +0900

    Member table and SignUp layout

--- src/views/member/Join.vue
+++ src/views/member/Join.vue
@@ -1,11 +1,22 @@
 <template>
-  <div> Join page </div>
+    <div class="d-flex justify-center align-center" style="height:100%">
+    <v-card max-width="400" width="100%" elevation="10">
+      <v-toolbar>
+        <v-toolbar-title>Sign Up</v-toolbar-title>
+      </v-toolbar>
+      <v-card-text> 
+				<sing-up-form/> 
+			</v-card-text>
+    </v-card>
+  </div>
 </template>
 
 <script>
+import SingUpForm from "../../components/auth/SignUpForm.vue";
 export default {
-
-}
+  components: { SingUpForm },
+  name: "Join",
+};
 </script>
 
 <style>
